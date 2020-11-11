122ad6964095f0d51e0b7b7f51f1116567a992f1
Eskinder <22ehaileyesus@athenian.org>
2020-11-11 16:36:04 +0300

    Stuck(need help)

--- my_code.py
+++ my_code.py
@@ -1,10 +1,19 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+#
 
 def avg_temp():
+    with open('temps.txt') as file_object:
+        line_list = file_object.readlines()
+        list_length = len(line_list)
+        sum_list = 0
+    for i in range(1, list_length):
+        line_list[i] = line_list[i].rstrip()
+        sum_list = int(line_list[i])+sum_list
+    print(sum_list)
+    average_list = sum_list/(list_length-1)
+    average_list = round(average_list, 2)
 
-
-    return
+    return average_list
 
 
 if __name__ == '__main__':
