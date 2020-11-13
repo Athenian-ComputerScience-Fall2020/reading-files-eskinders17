0bb8467f7742ae14c2c26896b0631e8fc7339a6f
eskinders17 <22ehaileyesus@athenian.org>
2020-11-13 15:57:33 +0000

    finished

--- file_practice.py
+++ file_practice.py
@@ -1,7 +1,7 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
 #
 
-
+#
 # Read temps.txt and print it without the blank line at the end
 with open('temps.txt') as file_object:
     contents = file_object.read()
